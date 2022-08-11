733c06597b96bb35b872f4d54e1ec90775653d36
julioaajr <61034583+julioaajr@users.noreply.github.com>
2022-08-11 16:08:19 -0300

    drf

--- nutri/models.py
+++ nutri/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+class Client (models.Model):
+    STATUS = (
+        ('0', 'Manhã'),
+        ('1', 'Tarde'),
+        ('2', 'Noite'),
+        ('3', 'Madrugada'),
+
+    )   
+    refeicao = models.TextField()
+    data_refeicao = models.DateTimeField(auto_now=True, verbose_name="Data da Refeição")
+    created_at = models.DateTimeField(auto_now=True, verbose_name="Criado em.")
+    created_by = models.ForeignKey(User, models.DO_NOTHING, blank = True, null = True, related_name="createdbyclient", verbose_name="Criado por")
