2b3eff831063fc635b8285e17b47e25e14f20a41
julioaajr <61034583+julioaajr@users.noreply.github.com>
2022-08-11 15:57:34 -0300

    drf

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+""""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))"
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('nutri.urls'))),
+    path('nutri/', include('nutri.urls')),
+    path('user/', include('user.urls')),
 ]
---
+++ user/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import *
+
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
